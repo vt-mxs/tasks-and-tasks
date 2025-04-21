2d520d573c4e00a162322bb6ba4e06759ca2267d
vt-mxs <maximusvitor88@gmail.com>
2025-04-21 12:10:37 -0300

    refactor: simplify user layer

--- src/main/java/com/etesc/tasksandtasks/api/controller/UserController.java
+++ src/main/java/com/etesc/tasksandtasks/api/controller/UserController.java
@@ -1,34 +1,31 @@
 package com.etesc.tasksandtasks.api.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.etesc.tasksandtasks.api.dto.request.UserRequestDTO;
-import com.etesc.tasksandtasks.api.dto.response.UserResponseDTO;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import com.etesc.tasksandtasks.api.dto.UserDTO;
 import com.etesc.tasksandtasks.api.service.UserService;
-
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+
 @Controller
-@RequestMapping("/user")
 public class UserController {
-	@Autowired
-	private UserService service;
-	
-	@GetMapping("/get/{email}")
-	public ResponseEntity<UserResponseDTO> getUserByEmail(@PathVariable String email){
-		UserResponseDTO response = service.getUserByEmail(email);
-		return ResponseEntity.ok(response);
-	}
-	
-	@PostMapping("/create")
-	public ResponseEntity<UserResponseDTO> createUser(@RequestBody @Valid UserRequestDTO request){
-		UserResponseDTO createdUser = service.createUser(request);
-		return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
-	}
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+        model.addAttribute("user", new UserDTO());
+        return "index";
+    }
+    
+    @PostMapping("/create/user")
+    public String handleUserRegistration(@ModelAttribute @Valid UserDTO form) {
+        UserDTO dto = new UserDTO(form.getName(), form.getEmail());
+        service.createUser(dto);
+        return "redirect:/";
+    }
+    
 }
--- src/main/java/com/etesc/tasksandtasks/api/service/UserService.java
+++ src/main/java/com/etesc/tasksandtasks/api/service/UserService.java
@@ -3,7 +3,7 @@ package com.etesc.tasksandtasks.api.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.etesc.tasksandtasks.api.dto.request.UserRequestDTO;
+import com.etesc.tasksandtasks.api.dto.UserDTO;
 import com.etesc.tasksandtasks.api.dto.response.UserResponseDTO;
 import com.etesc.tasksandtasks.api.repository.UserRepository;
 import com.etesc.tasksandtasks.model.User;
@@ -20,12 +20,12 @@ public class UserService {
 		return new UserResponseDTO(user.getId(), user.getName(), user.getEmail());
 	}
 	
-	public UserResponseDTO createUser(UserRequestDTO request) {
+	public UserResponseDTO createUser(UserDTO request) {
 		User user = new User();
-		user.setName(request.name());
-		user.setEmail(request.email());
+		user.setName(request.getName());
+		user.setEmail(request.getEmail());
 		user = repository.save(user);
 		
-		return new UserResponseDTO(user.getId(), request.name(), request.email());
+		return new UserResponseDTO(user.getId(), request.getName(), request.getEmail());
 	}
 }
