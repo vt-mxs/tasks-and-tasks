81c5222d8ea85dc05d72f89c8a33851fc2312b75
vt-mxs <maximusvitor88@gmail.com>
2025-04-23 20:59:22 -0300

    refactor: inital estrcuture to restAPI

--- src/main/java/com/etesc/tasksandtasks/api/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.etesc.tasksandtasks.api.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.etesc.tasksandtasks.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
--- src/main/java/com/etesc/tasksandtasks/api/repository/UserRepository.java
+++ src/main/java/com/etesc/tasksandtasks/api/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.etesc.tasksandtasks.api.repository;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.etesc.tasksandtasks.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Long>{
+    
 }
