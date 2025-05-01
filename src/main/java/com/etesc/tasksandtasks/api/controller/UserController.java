4173349556bd6fc4834998e46020191637dc9022
vt-mxs <maximusvitor88@gmail.com>
2025-05-01 14:33:16 -0300

    reafctor: padronize response

--- src/main/java/com/etesc/tasksandtasks/api/controller/UserController.java
+++ src/main/java/com/etesc/tasksandtasks/api/controller/UserController.java
@@ -7,6 +7,7 @@ import com.etesc.tasksandtasks.api.dto.response.UserResponseDTO;
 import com.etesc.tasksandtasks.api.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,14 +20,14 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDTO> getMethodName(@RequestBody @Valid UserLoginRequestDTO request) {
+    public ResponseEntity<UserResponseDTO> loginUser(@RequestBody @Valid UserLoginRequestDTO request) {
         UserResponseDTO response = service.loginUser(request);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/create") 
-    public ResponseEntity<UserResponseDTO> postMethodName(@RequestBody @Valid UserRequestDTO request) {
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody @Valid UserRequestDTO request) {
         UserResponseDTO response = service.createUser(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
