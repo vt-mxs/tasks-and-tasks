6f4831a45098fe60bae411cdb81b2f72110bcb3a
vt-mxs <maximusvitor88@gmail.com>
2025-05-02 10:57:04 -0300

    style: remove lines break

--- src/main/java/com/etesc/tasksandtasks/api/controller/TaskController.java
+++ src/main/java/com/etesc/tasksandtasks/api/controller/TaskController.java
@@ -1,16 +1,11 @@
 package com.etesc.tasksandtasks.api.controller;
-
 import org.springframework.web.bind.annotation.RestController;
-
 import com.etesc.tasksandtasks.api.dto.request.TaskRequestDTO;
 import com.etesc.tasksandtasks.api.dto.request.UserEmailRequestDTO;
 import com.etesc.tasksandtasks.api.dto.response.TaskResponseDTO;
 import com.etesc.tasksandtasks.api.service.TaskService;
-
 import jakarta.validation.Valid;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -40,5 +33,4 @@ public class TaskController {
 
         return ResponseEntity.ok(response);
     }
-    
 }
