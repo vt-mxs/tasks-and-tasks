2d5bf4910bb049322389d3e035e725f0511bf440
vt-mxs <maximusvitor88@gmail.com>
2025-06-28 12:40:10 -0300

    feat: enhance tasks display

--- src/main/resources/static/js/work-area-script/show-tasks-script.js
+++ src/main/resources/static/js/work-area-script/show-tasks-script.js
@@ -17,31 +17,45 @@ async function loadTasks() {
     return await response.json();
 }
 
+async function formatTasks(tasks) {
+    let tab = '';
+    tasks.forEach(task => {
+        tab += `
+            <tr class="task">
+                <th>${task.title}</th>
+                <th>${task.description}</th>
+                <th>${((task.completed)? "Sim" : "Não")}</th>
+                <th>${task.category.name}</th>
+                <th>${task.priority.level}</th>
+                <th>
+                    <button class="button">Concluir</button>
+                    <button class="button">Editar</button>
+                    <button class="button">Excluir</button>
+                </th>
+            </tr>
+        `;
+    });
+    document.getElementById("tasks-table").innerHTML += tab;
+}
+
 async function showTasks() {
     try {
         const data = await loadTasks();
+        console.log(data);
         
-        const taskList = document.getElementById("task-list");
-        let tasksLis = [];
-
-        data.forEach(task => {
-            const taskElement = document.createElement("li");
-            taskElement.innerText = `Titulo: ${task.title} | Descrição: ${task.description} | Completada: ${(task.completed)? "Sim" : "Não"} | Categoria: ${task.category.name} | Prioridade: ${task.priority.level}`;
-            tasksLis.push(taskList.appendChild(taskElement));
-        });
-        return tasksLis;
+        formatTasks(data);
+        // const taskList = document.getElementById("task-list");
+        
+        
+        // data.forEach(task => {
+        //     const taskElement = document.createElement("li");
+        //     taskElement.innerText = `Titulo: ${task.title} | Descrição: ${task.description} | Completada: ${(task.completed)? "Sim" : "Não"} | Categoria: ${task.category.name} | Prioridade: ${task.priority.level}`;
+        //     tasksLis.push(taskList.appendChild(taskElement));
+        // });
+        // return tasksLis;
     } catch (error) {
         window.alert(`Problemas técnicos :( ${error}`);
     }
 }
 
-async function formatTasks() {
-    const tasksLis = await showTasks();
-    tasksLis.forEach(taskLi => {
-        taskLi.style.listStyle = "none";
-        taskLi.style.color = "#fff"
-        taskLi.style.padding = "2%";
-    });
-}
-
-formatTasks();
+showTasks();
